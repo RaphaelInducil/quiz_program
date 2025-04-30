d2532e39752aa6ee6ed45c5f072033d64e9017b3
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-04-30 23:30:56 +0800

    check point: user input file, wiht open file (r), read lines of file, chop the files into blocks (qstn, a, b, c, d), append quiz data, and shuffle quiz data.

--- quiz_program.py
+++ quiz_program.py
@@ -5,13 +5,48 @@
 # Create the Quiz program that read the output file of the Quiz Creator. The user will answer the randomly selected question and check if the answer is correct.
 
 # user input for file (txt) name of what to read
+
+quiz_file = input("Enter the name of your quiz file (without .txt): ") + ".txt"
+
 # open with read mode 
+
+with open(quiz_file, "r") as file:
+    file_lines = file.readlines()
+
 # add new variable to read file line using .readlines
 # initialize empty list for questions and lines
+
+quiz_data = []
+line_number = 0
+
 # while loop file and collect the questions and multiple choice answers
 # grab 6 lines from file (qstn, a, b, c, d, ca)
+
+while line_number < len(file_lines):
+    if file_lines[line_number}.startswith("Question: ")]
+        question = file_lines[line_number].strip()
+        answer_a = file_lines[line_number + 1].strip()
+        answer_b = file_lines[line_number + 2].strip()
+        answer_c = file_lines[line_number + 3].strip()
+        answer_d = file_lines[line_number + 4].strip()
+        correct_answer = file_lines[line_number + 5].strip()
+
 # append to the list of questions and answers
+
+        quiz_data.append({
+            question_line,
+            answer_a,
+            answer_b,
+            answer_c,
+            answer_d,
+            correct_answer
+        })
+        line_number += 7
+
 # shuffle the list of questions and answers
+
+random.shuffle(quiz_data)
+
 # display the question and answers to the user
 # ask user for input of the answer
 # get the correct answer put it in a new variable
