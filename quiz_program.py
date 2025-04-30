4f1b2fa477ba83ae463cffa08dee492932b3c96e
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-04-30 23:12:28 +0800

    pseudocode

---
+++ quiz_program.py
@@ -0,0 +1,21 @@
+# raphael clouiee inducil
+# BSCpE 1-2 
+# 04-07-25
+# assignment 10: quiz
+# Create the Quiz program that read the output file of the Quiz Creator. The user will answer the randomly selected question and check if the answer is correct.
+
+# user input for file (txt) name of what to read
+# open with read mode 
+# add new variable to read file line using .readlines
+# initialize empty list for questions and lines
+# while loop file and collect the questions and multiple choice answers
+# grab 6 lines from file (qstn, a, b, c, d, ca)
+# append to the list of questions and answers
+# shuffle the list of questions and answers
+# ask user for input of the answer
+# get the correct answer put it in a new variable
+# compare using if to user's answer
+# if correct, print correct
+# else print wrong and show the correct answer
+# loop to ask another question
+# done
